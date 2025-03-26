8a93f2dbc447d86dbf0362d19dafb9ce457b9324
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-03-26 13:09:14 +0545

    thread sleep stop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+//import java.util.function.Function;
+
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,11 +14,43 @@ public class Main {
         t1.start();
 
 
+//        try{
+//   t1.stop();
+//        }
+//        catch (Exception ex){
+//            System.out.println(ex);
+//        }
 
-        ThreadRunnable threadRunnable=new ThreadRunnable();
-        Thread thread=new Thread(threadRunnable);
-        thread.start();
 
+
+
+        new ThreadRunnable("1st");
+        new ThreadRunnable("2nd");
+        new ThreadRunnable("3rd");
+
+        try{
+            Thread.sleep(8000);
+        }
+        catch (InterruptedException ex){
+            System.out.println(ex);
+        }
+
+
+
+
+//        ThreadRunnable threadRunnable=new ThreadRunnable();
+//        Thread thread=new Thread(threadRunnable);
+//        thread.start();
+
+        List<String>list=new ArrayList<String>();
+
+        list.add("shakti");
+        list.add("anu");
+        list.add("aman");
+
+//        not supportged
+
+//        Function<Long,Long> am= a->a+7;
+        }
 
     }
-}
--- src/com/company/ThreadRunnable.java
+++ src/com/company/ThreadRunnable.java
@@ -1,8 +1,18 @@
 package com.company;
 
 public class ThreadRunnable implements Runnable {
+Thread thread;
+String name;
+    ThreadRunnable(String name){
+        this.name=name;
+
+        Thread thread=new Thread(this,name);
+        System.out.println("New Thread"+"\t"+thread + "\n");
+    }
     @Override
     public void run() {
-        System.out.println("By world");
+
+
+
     }
 }
