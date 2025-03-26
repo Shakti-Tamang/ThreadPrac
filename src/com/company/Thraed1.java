39adbbdcbdfe44f56654109a08f1d6110a50065f
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-03-26 21:58:48 +0545

    sync use in thread

--- src/com/company/Thraed1.java
+++ src/com/company/Thraed1.java
@@ -7,7 +7,7 @@ public class Thraed1 extends Thread {
         System.out.println("Hello");try {
             for (int i = 0; i < 44; i++) {     System.out.println("Thread Starting");
                 System.out.println(i);
-                Thread.sleep(000);
+                Thread.sleep(1000);
             }
         }
         catch (Exception ex){
